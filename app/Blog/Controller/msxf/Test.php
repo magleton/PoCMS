bf2bd64251dc6244233d0cae2695d4d67d32974d
macro <869158081@qq.com>
2016-08-26 23:04:19 +0800

    重命名文件夹名字

---
+++ app/Blog/Controller/msxf/Test.php
@@ -0,0 +1,14 @@
+<?php
+namespace Blog\controller\msxf;
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/6/27
+ * Time: 19:13
+ */
+class Test extends \Controller\Controller
+{
+    public function abc(){
+        echo "aaaa";
+    }
+}
