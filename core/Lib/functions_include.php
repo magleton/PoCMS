3193f4321ad2d4149865dabdcb4de9f05ea647f7
macro <869158081@qq.com>
2016-09-22 20:44:36 +0800

    修改Doctrine命令行的启动

--- core/Lib/functions_include.php
+++ core/Lib/functions_include.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . "/functions.php";
 require_once __DIR__ . "/slog.function.php";
-if (file_exists(APP_PATH . '/Lib/func.inc.php')) {
+if (defined('APP_PATH') && file_exists(APP_PATH . '/Lib/func.inc.php')) {
     require_once APP_PATH . '/Lib/func.inc.php';
 }
