9f09e8b17de5c87b2fb996873fc53c0224abdc80
macro <macro_fengye@163.com>
2016-11-08 10:55:09 +0800

    修正单元测试文件启动文件

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,9 @@
 <?php
-//单元测试的引导文件
 date_default_timezone_set('Asia/Shanghai');
 defined('APPLICATION_ENV') || define('APPLICATION_ENV', 'development');
 define('ROOT_PATH', dirname(__DIR__));
-define('APP_NAME', 'task');
-define('APP_PATH', ROOT_PATH . '/' . APP_NAME . '/');
-define('CONFIG_PATH', ROOT_PATH . '/' . APP_NAME . '/');
+define('APP_NAME', 'Blog');
+define('APP_PATH', ROOT_PATH . '/app/' . APP_NAME . '/');
 require ROOT_PATH . '/vendor/autoload.php';
 $app = new \Core\Boot\Application();
 $app->startConsole();
