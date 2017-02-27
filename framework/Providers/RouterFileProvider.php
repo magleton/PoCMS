8f0bf2ed72f08a7f71e169f817eb7a83daae5fa7
macro <macro_fengye@163.com>
2017-02-27 10:51:25 +0800

    修复生成路由文件添加中间件的BUG!

--- framework/Providers/RouterFileProvider.php
+++ framework/Providers/RouterFileProvider.php
@@ -25,13 +25,17 @@ class RouterFileProvider implements ServiceProviderInterface
     {
         $pimple['routerFile'] = function (Container $container) {
             if (APPLICATION_ENV === 'development' || !file_exists(APP_PATH . '/Routers/router.lock')) {
-                if (file_exists($container['application']->config('customer.router_cache_file'))) @unlink($container['application']->config('customer.router_cache_file'));
+                if (file_exists($container['application']->config('customer.router_cache_file'))) {
+                    @unlink($container['application']->config('customer.router_cache_file'));
+                }
                 $router_file_contents = '<?php ' . "\n" . '$app = $container[\'application\']->component(\'app\');';
-                $router_file_contents .= "\n".'$app->add(new \RunTracy\Middlewares\TracyMiddleware($app))';
+                $router_file_contents .= "\n" . '$app->add(new \RunTracy\Middlewares\TracyMiddleware($app))';
                 if ($container['application']->config('middleware')) {
-                    foreach ($container['application']->config('middleware') as $middleware) {
+                    foreach ($container['application']->config('middleware') as $key => $middleware) {
                         if (function_exists($middleware) && is_callable($middleware)) {
                             $router_file_contents .= '->add("' . $middleware . '")';
+                        } elseif ($container['application']->component($key)) {
+                            $router_file_contents .= '->add($container[\'application\']->component("' . $key . '"))';
                         } elseif (class_exists($middleware)) {
                             $router_file_contents .= '->add("' . $middleware . '")';
                         }
