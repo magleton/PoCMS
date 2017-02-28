ebdb1d7f0a95e7e1253d88dffdba4498a22b912a
macro <869158081@qq.com>
2017-02-28 21:53:51 +0800

    基类新增魔术方法与新增属性方法

--- framework/Repository/Repository.php
+++ framework/Repository/Repository.php
@@ -125,4 +125,70 @@ class Repository extends EntityRepository
         }
         return $this->presenterInstance;
     }
+
+    /**
+     * 给对象新增属性
+     *
+     * @param $propertyName
+     * @param $value
+     * @return $this
+     */
+    protected function setProperty($propertyName, $value)
+    {
+        if (!isset($this->$propertyName)) {
+            $this->$propertyName = $value;
+        }
+        return $this;
+    }
+
+    /**
+     * 获取对象属性
+     *
+     * @param $propertyName
+     * @return mixed
+     */
+    protected function getProperty($propertyName)
+    {
+        if (!isset($this->$propertyName)) {
+            return $this->$propertyName;
+        }
+        return null;
+    }
+
+    /**
+     * is utilized for reading data from inaccessible members.
+     *
+     * @param $name string
+     * @return mixed
+     * @link http://php.net/manual/en/language.oop5.overloading.php#language.oop5.overloading.members
+     */
+    public function __get($name)
+    {
+        return isset($this->$name) ? $this->$name : null;
+    }
+
+    /**
+     * run when writing data to inaccessible members.
+     *
+     * @param $name string
+     * @param $value mixed
+     * @return $this
+     * @link http://php.net/manual/en/language.oop5.overloading.php#language.oop5.overloading.members
+     */
+    public function __set($name, $value)
+    {
+        $this->$name = $value;
+    }
+
+    /**
+     * is triggered by calling isset() or empty() on inaccessible members.
+     *
+     * @param $name string
+     * @return bool
+     * @link http://php.net/manual/en/language.oop5.overloading.php#language.oop5.overloading.members
+     */
+    public function __isset($name)
+    {
+        return isset($this->$name);
+    }
 }
