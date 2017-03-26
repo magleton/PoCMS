a9ae40e2762fd09337a0ae23429aefa7e772a90e
macro <869158081@qq.com>
2017-03-26 12:13:21 +0800

    新增配置服务供给者的命名空间配置!

--- app/WeiXin/Config/customer.config.php
+++ app/WeiXin/Config/customer.config.php
@@ -1,6 +1,9 @@
 <?php
 //自定义配置
 $customer_config = [
+    'app' => [
+        'providersPath' => ['WeChat\\Providers']
+    ],
     'middleware' => [
         'initAccessToken',
         //'ip_filter' => \WeChat\Providers\IpFilterProvider::class,
