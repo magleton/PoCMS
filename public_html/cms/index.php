741804015fc7ab4870965a2ba306a7e8dbcd5ded
macro <macro_fengye@163.com>
2017-03-27 14:35:40 +0800

    修复php5下报错!

--- public_html/cms/index.php
+++ public_html/cms/index.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set('Asia/Shanghai');
-define('ROOT_PATH', dirname(__DIR__, 2));
+define('ROOT_PATH', dirname(dirname(__DIR__)));
 define('APP_NAME', 'CMS');
 define('TEMPLATE_PATH', ROOT_PATH . '/app/' . APP_NAME . '/Templates/');
 define('APP_PATH', ROOT_PATH . '/app/' . APP_NAME . '/');
