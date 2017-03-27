45a43bdd771a9e6dd28a042ea28959764c38ea12
macro <macro_fengye@163.com>
2017-03-27 13:51:01 +0800

    修正Php cs的警告信息!

--- public_html/weixin/index.php
+++ public_html/weixin/index.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set('Asia/Shanghai');
-define('ROOT_PATH', dirname(dirname(__DIR__)));
+define('ROOT_PATH', dirname(__DIR__, 2));
 define('APP_NAME', 'WeiXin');
 define('TEMPLATE_PATH', ROOT_PATH . '/app/' . APP_NAME . '/Templates/');
 define('APP_PATH', ROOT_PATH . '/app/' . APP_NAME . '/');
