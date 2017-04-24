74c48e70b15c9b946fa3df8f8d4575254094b6f8
macro <869158081@qq.com>
2017-04-24 19:39:11 +0800

    修改目录分隔符!

--- public_html/cms/index.php
+++ public_html/cms/index.php
@@ -1,9 +1,10 @@
 <?php
 date_default_timezone_set('Asia/Shanghai');
+define('DS', DIRECTORY_SEPARATOR);
 define('ROOT_PATH', dirname(dirname(__DIR__)));
 define('APP_NAME', 'CMS');
-define('TEMPLATE_PATH', ROOT_PATH . '/app/' . APP_NAME . '/Templates/');
-define('APP_PATH', ROOT_PATH . '/app/' . APP_NAME . '/');
+define('TEMPLATE_PATH', ROOT_PATH . DS . 'app' . DS . APP_NAME . DS . 'Templates' . DS);
+define('APP_PATH', ROOT_PATH . DS . 'app' . DS . APP_NAME . DS);
 require ROOT_PATH . '/vendor/autoload.php';
 defined('APPLICATION_ENV') || define('APPLICATION_ENV', 'development');
 $app = new \Polymer\Boot\Application();
