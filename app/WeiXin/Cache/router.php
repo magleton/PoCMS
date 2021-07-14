315fe41c157854e10e915a97ff74c247b994f0f4
britton <britton@126.com>
2021-07-14 18:33:05 +0800

    代码重构

--- app/WeiXin/Cache/router.php
+++ app/WeiXin/Cache/router.php
@@ -2,11 +2,12 @@
 use Polymer\Boot\Application;
 use Slim\App;
 $app = Application::getInstance()->getDiContainer()->get(App::class);
-$app->map(['GET', 'POST'], '/', APP_NAME . '\\Controller\\Home:index')->setName('wechat.home.root.index');
+$app->add(Application::getInstance()->getDiContainer()->get("corsMiddleware"));
+$app->get('/admin', APP_NAME . '\\Controller\\Backend\\AdminController:test')->setName('admin.user.test');
+
+$app->GET('/', APP_NAME . '\\Controller\\Home:index')->setName('wechat.home.root.index');
 $app->map(['GET', 'POST'], '/home/add-user', APP_NAME . '\\Controller\\Home:addUser')->setName('wechat.home.add.user');
-$app->map(['GET', 'POST'], '/home/index', APP_NAME . '\\Controller\\Home:index')->setName('wechat.home.index');
 $app->map(['GET', 'POST'], '/home/send', APP_NAME . '\\Controller\\Home:send')->setName('wechat.home.send');
-$app->post('/console', 'RunTracy\Controllers\RunTracyConsole:index');
 $app->map(['GET', 'POST'], '/oauth/oAuthWebsite', APP_NAME . '\\Controller\\OAuth:oAuthWebsite')->setName('wechat.OAuth.oAuthWebsite');
 $app->map(['GET', 'POST'], '/oauth/dealOAuthWebsite', APP_NAME . '\\Controller\\OAuth:dealOAuthWebsite')->setName('wechat.OAuth.dealOAuthWebsite');
 
